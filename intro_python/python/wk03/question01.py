b184eb718703304cfcfe5d5457fd3f8d211e5bf6
Steve Williams <frodas.ham@gmail.com>
2018-10-15 19:47:47 +0100

    my code

--- intro_python/python/wk03/question01.py
+++ intro_python/python/wk03/question01.py
@@ -1,13 +1,20 @@
 # A simple Y/N validation function
 # q_text = Question text
-q_txt = 'Are we learning any python yet'
+#q_txt = 'Are we learning any python yet'
 
-def get_answer (q_text):
-    return True
-
+def get_answer (question):
+    gotanswer = False
+    while gotanswer == False:
+        x = str(input(question + '? [Y/N]: ')) 
+        if x.upper() == 'Y':
+            return True
+            gotanswer = True
+        elif x.upper() == 'N':
+            return False
+            gotanswer = True
 
 # Ask question and define result
-if get_answer(q_txt):
+if get_answer('Are we learning any python yet'):
     print('Yes')
 else:
     print('No')
--- intro_python/python/wk03/question02.py
+++ intro_python/python/wk03/question02.py
@@ -4,14 +4,22 @@ data = {"Q": "Is it an aquatic animal"}
 # A simple Y/N validation function
 # q_text = Question text
 # Get question from data
-q_txt = ' '
 
-def get_answer (q_text):
-    return True
+def get_answer (question):
+    gotanswer = False
+    while gotanswer == False:
+        x = str(input(question + '? [Y/N]: ')) 
+        if x.upper() == 'Y':
+            return True
+            gotanswer = True
+        elif x.upper() == 'N':
+            return False
+            gotanswer = True
+
 
 
 # Ask question and define result
-if get_answer(q_txt):
+if get_answer(data['Q']):
     print('YES')
 else:
     print('No')
